d7687905fc9dd51e5823425693bf560f8bed602f
Your Name <you@example.com>
2023-06-15 13:51:41 +0600

    employeeecreate

--- src/Component/Employee/CreateEmployee.js
+++ src/Component/Employee/CreateEmployee.js
@@ -1,9 +1,50 @@
+import { Box, TextField } from '@mui/material';
 import React from 'react';
 
 const CreateEmployee = () => {
+
+  
     return (
         <div>
-            
+         <h1 style={{textAlign:'center', font:'30px'}}>Employee Create</h1>
+             <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { mt: 20, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    />
+      <div style={{marginTop: '30px', display:'grid', gridColumn:'2', width:'30%', 
+       marginLeft:'auto', marginRight:'auto', gap: '20px' }}>
+        <TextField
+          required
+          id="outlined-required"
+          label="First Name"
+         
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Last name"
+          
+        />
+        <TextField
+          id="outlined-disabled"
+          label="EmpID"
+          type='number'
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Division"
+          type="text"
+         
+        />
+        <TextField
+          id="outlined-read-only-input"
+          label="District"
+        />
+        </div>
         </div>
     );
 };
--- src/Component/Employee/Employee.js
+++ src/Component/Employee/Employee.js
@@ -1,5 +1,5 @@
 import {Button} from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import {Link} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
@@ -33,17 +33,7 @@ const Employee = () => {
     },
   ];
 
-  const rows = [
-    {id: 1, lastName: "Snow", firstName: "Jon", age: 35},
-    {id: 2, lastName: "Lannister", firstName: "Cersei", age: 42},
-    {id: 3, lastName: "Lannister", firstName: "Jaime", age: 45},
-    {id: 4, lastName: "Stark", firstName: "Arya", age: 16},
-    {id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null},
-    {id: 6, lastName: "Melisandre", firstName: null, age: 150},
-    {id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44},
-    {id: 8, lastName: "Frances", firstName: "Rossini", age: 36},
-    {id: 9, lastName: "Roxie", firstName: "Harvey", age: 65},
-  ];
+
   const {data: Employeelist = []} = useQuery({
     queryKey: ["allUsers"],
     queryFn: async () => {
@@ -67,9 +57,9 @@ const Employee = () => {
           color: "black",
           display: "flex",
           placeContent: "center",
-          outline: "none",
+          outlineColor: 'black'
         }}
-        to="/employee">
+        to="/createEmployee">
         <Button
           size="large"
           variant="elevated"
@@ -89,7 +79,8 @@ const Employee = () => {
       
     }}
       
-      rows={ employee && employee.map((data , i) => data)}
+      
+      rows = {Employeelist.readEmployeeData}
       columns={columns}
       initialState={{
         pagination: {
